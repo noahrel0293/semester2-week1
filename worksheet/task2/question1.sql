3adaa768d4e93c76aef2e62b81ab46a9e35432df
noahrel0293 <gxnd0293@leeds.ac.uk>
2026-02-23 17:56:24 +0000

    second commit

--- worksheet/task2/question1.sql
+++ worksheet/task2/question1.sql
@@ -2,5 +2,4 @@
 -- Expected Columns:
 -- StudentId, FirstName, LastName, EnrolmentYear
 
-CREATE TABLE 2022Students(StudentId, FirstName, LastName, EnrolmentYear);
-INSERT INTO 2022Students SELECT StudentId, FirstName, LastName, EnrolmentYear FROM student.db WHERE EnrolmentYear=2022;
+SELECT StudentId, FirstName, LastName, EnrolmentYear FROM student.db WHERE EnrolmentYear=2022;
