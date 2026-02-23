8f88f54862b05e729ea50826381eba885a9ca8a0
noahrel0293 <gxnd0293@leeds.ac.uk>
2026-02-23 17:54:21 +0000

    First Commit

--- worksheet/task2/question1.sql
+++ worksheet/task2/question1.sql
@@ -1,3 +1,6 @@
 -- List all students who enrolled in the year 2022.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, EnrolmentYear
+
+CREATE TABLE 2022Students(StudentId, FirstName, LastName, EnrolmentYear);
+INSERT INTO 2022Students SELECT StudentId, FirstName, LastName, EnrolmentYear FROM student.db WHERE EnrolmentYear=2022;
