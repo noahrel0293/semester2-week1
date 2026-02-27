7e890236ccada19e111c2c2893ce67a71b6d1843
noahrel0293 <gxnd0293@leeds.ac.uk>
2026-02-27 23:10:02 +0000

    fourth commit

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,4 +1,5 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
-SELECT DepartmentName, COUNT(CourseId) AS TotalEnrolments FROm department JOIN enrolment 
+sqlite3 student.db
+SELECT DepartmentName, COUNT(CourseId) AS TotalEnrolments FROM ((Enrolment INNER JOIN Course ON Enrolment.CourseId = Course.CourseId) INNER JOIN Department ON Enrolment.DepartmentId = Department.DepartmentId) GROUP BY DepartmentName;
